fc8d1de132367451a3b991e7197f29ef98480fe6
mswindler <matthew@ionixdesign.com>
2017-11-30 14:10:52 -0700

    Create Votes.js

---
+++ midterm2/models/Votes.js
@@ -0,0 +1,12 @@
+var mongoose = require('mongoose');
+var VoteSchema = new mongoose.Schema({
+  name: String,
+  votes: {type: Number, default: 0},
+});
+
+VoteSchema.methods.upvote = function(cb) {
+ this.votes += 1;
+ this.save(cb);
+};
+
+mongoose.model('Vote', VoteSchema);
