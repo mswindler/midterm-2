d73235ca48eda0e6077d4d2ff72c0e49bc71276c
mswindler <matthew@ionixdesign.com>
2017-11-30 14:12:39 -0700

    Update app.js

--- midterm2/app.js
+++ midterm2/app.js
@@ -4,13 +4,14 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/votesDB', {useMongoClient:true});
+require('./models/Votes');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
-var app = express();
-
-// view engine setup
+var app = express();// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
