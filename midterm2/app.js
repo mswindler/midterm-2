5942c5bad0afc5cebc67be461f7b8feb1f71c4e9
mswindler <matthew@ionixdesign.com>
2017-11-30 15:11:52 -0700

    Update app.js

--- midterm2/app.js
+++ midterm2/app.js
@@ -11,7 +11,9 @@ require('./models/Items');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
-var app = express();// view engine setup
+var app = express();
+
+// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
