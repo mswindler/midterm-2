39ed25349de88f8087996a8f4eac74a1addbfdfe
mswindler <matthew@ionixdesign.com>
2017-11-30 15:11:31 -0700

    Update app.js

--- midterm2/app.js
+++ midterm2/app.js
@@ -5,7 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/itemsDB', {useMongoClient:true});
+mongoose.connect('mongodb://localhost/itemsDB');
 require('./models/Items');
 
 var routes = require('./routes/index');
