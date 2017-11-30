73fb459869dfc56a0bc67a7d4097b5496ff4908b
mswindler <matthew@ionixdesign.com>
2017-11-30 14:20:41 -0700

    Update Votes.js

--- midterm2/models/Votes.js
+++ midterm2/models/Votes.js
@@ -1,11 +1,12 @@
 var mongoose = require('mongoose');
 var VoteSchema = new mongoose.Schema({
   name: String,
-  votes: {type: Number, default: 0},
+  quantity: {type: Number, default: 0},
+  cost: Number,
 });
 
 VoteSchema.methods.upvote = function(cb) {
- this.votes += 1;
+ this.quantity += 1;
  this.save(cb);
 };
 
